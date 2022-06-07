cf193a3db324359bd11087a4c950ef430581db46
superman <duonga2qp@gmail.com>
2022-06-07 14:58:15 +0700

    nopbai

---
+++ [Thuc-hanh]-Xay-dung-chuc-nang-them-moi-thong-tin-khach-hang/server.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const fs = require('fs')
+const qs = require('qs')
+
+const server = http.createServer((req, res) => {
+    if(req.method ==='GET') {
+        fs.readFile('./templates/create.html', 'utf8', (err, data) => {
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data);
+            return res.end()
+        })
+    }else{
+        fs.readFile('./templates/create.html','utf-8',(err, data) => {
+            let dataHTML = ''
+            req.on('data',chunk=>{
+                dataHTML+=chunk
+            })
+            req.on('end',()=>{
+                let name = qs.parse(dataHTML)
+                console.log(name)
+                fs.writeFile('./data/data',JSON.stringify(name),err=>{
+                    if(err){
+                        console.log('err')
+                        return
+                    }
+                    return res.end()
+                })
+            })
+        })
+    }
+})
+server.listen(3000, function (){
+    console.log('Serve running port 3000')
+})
